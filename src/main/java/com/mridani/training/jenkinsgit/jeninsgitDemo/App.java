4dbb29f4fa757df67532fefc7a122497e320f456
mridani-technologies <mridanitechnologies@gmail.com>
2020-08-08 20:19:24 +0530

    jenkins git demo

---
+++ src/main/java/com/mridani/training/jenkinsgit/jeninsgitDemo/App.java
@@ -0,0 +1,13 @@
+package com.mridani.training.jenkinsgit.jeninsgitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
